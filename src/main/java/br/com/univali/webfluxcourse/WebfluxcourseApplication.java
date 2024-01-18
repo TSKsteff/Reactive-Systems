d8ecc001d3fc321c16709cc9bf433c3902a8d192
TSKsteff <steff@edu.univali.br>
2024-01-18 16:27:43 -0300

    Start project

---
+++ src/main/java/br/com/univali/webfluxcourse/WebfluxcourseApplication.java
@@ -0,0 +1,13 @@
+package br.com.univali.webfluxcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxcourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxcourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/univali/webfluxcourse/WebfluxcourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.univali.webfluxcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxcourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
